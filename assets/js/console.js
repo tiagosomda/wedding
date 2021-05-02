bb00840c0974b3c735ced9ed081ec25ed7f0545c
tiagosomda <tsomda@gmail.com>
2021-05-02 02:03:10 -0700

    updated console message

--- assets/js/console.js
+++ assets/js/console.js
@@ -6,4 +6,8 @@ _|_|_|    _|_|      _|      _|      _|
 _|        _|        _|      _|      _|  
 _|        _|        _|      _|      _|  
 
+source code: https://github.com/tiagosomda/wedding/
+
+???? a mistery...
+aGEhIHlvdSBoYXZlIGZpZ3VyZWQgb3V0IHRoZSBzZWNyZXQhIENvbmdyYXR1bGF0aW9ucyEgU2VuZCBtZSBhbiBlbWFpbCBhdCBzZWNyZXRAcHJpdGkuZmFtaWx5IHRvIGxldCBtZSBrbm93IDop
 `);
